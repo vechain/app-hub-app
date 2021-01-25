fbeaf6cc910a5ee320afce2d838837279029c570
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-25 03:27:55 +0000

    Deploy apps.vechain.org to github.com/vechain/app-hub-app.git:gh-pages

---
+++ precache-manifest.728937bdf4a33838bd6f2db18aad303e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "8656aed521da8752dfd8",
+    "url": "/css/app.8f430c74.css"
+  },
+  {
+    "revision": "8656aed521da8752dfd8",
+    "url": "/js/app.e8dc2d00.js"
+  },
+  {
+    "revision": "d7233ee3fde8c23da466",
+    "url": "/css/chunk-vendors.fa7d0e5c.css"
+  },
+  {
+    "revision": "d7233ee3fde8c23da466",
+    "url": "/js/chunk-vendors.e1717f9f.js"
+  },
+  {
+    "revision": "2b44a9bb435e238743804c85a2b297f4",
+    "url": "/img/com.vechain.tokens.2b44a9bb.png"
+  },
+  {
+    "revision": "1171a6a7122fcce28f52a15ca2097eb9",
+    "url": "/img/com.vechainstats.1171a6a7.png"
+  },
+  {
+    "revision": "0feb329413b54e916696246fe7af82f0",
+    "url": "/img/org.vepress.vepress.0feb3294.png"
+  },
+  {
+    "revision": "c7d98cc4e1742f4087c8085a2a9a16e7",
+    "url": "/img/com.vns.vns.c7d98cc4.png"
+  },
+  {
+    "revision": "e322d45d12a7f39def4b389d5f12bf53",
+    "url": "/img/com.vechainstats.manager.e322d45d.png"
+  },
+  {
+    "revision": "a3afcf7bfb73910d6f712c34f9e55cca",
+    "url": "/img/com.vechainstats.wallu.a3afcf7b.png"
+  },
+  {
+    "revision": "c5c7f935831497cb0cc36f3c335b70a2",
+    "url": "/img/com.vechain.inspector.c5c7f935.png"
+  },
+  {
+    "revision": "c8d8a9277bfd3eaf8136531b77beb320",
+    "url": "/img/org.realitems.c8d8a927.png"
+  },
+  {
+    "revision": "5b9cf491e79d47c158776e561efe350f",
+    "url": "/img/com.vechain.insight.5b9cf491.png"
+  },
+  {
+    "revision": "b37ae9e98b7e3f8dbb62e778971ff4ed",
+    "url": "/img/com.vechain.bmac.b37ae9e9.png"
+  },
+  {
+    "revision": "ab10a52a47496f341f7b258280a373a3",
+    "url": "/img/io.vechainworld.docs.ab10a52a.png"
+  },
+  {
+    "revision": "3be1b8d3de338a71cc2076993e85df19",
+    "url": "/img/com.laalaguer.token-transfer.3be1b8d3.png"
+  },
+  {
+    "revision": "1bd7dcdff431115d001161d68390a825",
+    "url": "/img/com.bc66.lucky-airdrop.1bd7dcdf.png"
+  },
+  {
+    "revision": "bef87d057543bb705a910ae56cdd9202",
+    "url": "/img/io.thorblock.portal.bef87d05.png"
+  },
+  {
+    "revision": "179459d5410eed9167e21bbd80675cbf",
+    "url": "/img/com.vechain.vevote.179459d5.png"
+  },
+  {
+    "revision": "093fbe0afdd58d224367778ab09b53cb",
+    "url": "/img/com.2048x.2048x.093fbe0a.png"
+  },
+  {
+    "revision": "28b8500765060951e20fb62a0e4d599a",
+    "url": "/img/com.vechainstore.ide.28b85007.png"
+  },
+  {
+    "revision": "9b31893b5a93e23696a5637a1fc56731",
+    "url": "/img/com.everprotocol.everscore.9b31893b.png"
+  },
+  {
+    "revision": "44fc581b86bde3c917aefaf86df5372f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
