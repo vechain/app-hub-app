996d0f91c7bd7dbe4c6cc4a0b6f179417ee28fb1
AsbertMa <stayinalive@msn.cn>
2022-02-21 17:00:51 +0800

    icons & pwa config

--- vue.config.js
+++ vue.config.js
@@ -13,13 +13,24 @@ module.exports = {
     ]
   },
   pwa: {
+    name: 'App Hub',
     themeColor: '#eee',
     workboxOptions: {
       skipWaiting: true,
       clientsClaim: true
     },
+<<<<<<< HEAD
     name: 'VeChain Apps',
     appleMobileWebAppCapable: 'yes',
     themeColor: '#2f353b'
+=======
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: null,
+      msTileImage: 'img/icons/mstitle-150.png'
+    }
+>>>>>>> 836f71a (icons & pwa config)
   }
 }
