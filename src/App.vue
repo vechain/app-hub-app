85ca5fe3cc3f063fadeaa3c2594a032aad016fbf
ManuFala <126077542+ManuFala@users.noreply.github.com>
2023-02-25 15:34:20 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -167,7 +167,7 @@ export default class App extends Vue {
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Poppins", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   margin: auto;
