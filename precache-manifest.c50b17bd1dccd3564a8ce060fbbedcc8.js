e744eb6086aff86918049a69f5303d0b49366619
libotony <libotony@users.noreply.github.com>
2024-03-28 15:54:56 +0000

    deploy: vechain/app-hub@a14d464a66c6fc751327a08cd3b7028fd014881c

---
+++ precache-manifest.c50b17bd1dccd3564a8ce060fbbedcc8.js
@@ -0,0 +1,406 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b66597b2a54e69cfa1c",
+    "url": "/css/app.e148f40c.css"
+  },
+  {
+    "revision": "ebeca7bfc5b2ee802d43",
+    "url": "/css/chunk-vendors.ec32ade2.css"
+  },
+  {
+    "revision": "689b8ed6221f443f1ee725dc65afc806",
+    "url": "/img/app.vercel.vet-token-transfer.689b8ed6.png"
+  },
+  {
+    "revision": "7a5a8fc1b465167bc59c80e8fb92dc45",
+    "url": "/img/art.squadvechain.7a5a8fc1.png"
+  },
+  {
+    "revision": "093fbe0afdd58d224367778ab09b53cb",
+    "url": "/img/com.2048x.2048x.093fbe0a.png"
+  },
+  {
+    "revision": "1bd7dcdff431115d001161d68390a825",
+    "url": "/img/com.bc66.lucky-airdrop.1bd7dcdf.png"
+  },
+  {
+    "revision": "3dc69ec59dc011cd33051d31514274bc",
+    "url": "/img/com.blackvemarket.3dc69ec5.png"
+  },
+  {
+    "revision": "e9d5138977ddd7ba45f56e15de45a106",
+    "url": "/img/com.defibetclub.e9d51389.png"
+  },
+  {
+    "revision": "28b849edd4ba5d519a0bb34be5b4252f",
+    "url": "/img/com.dragonsofsingapura.28b849ed.png"
+  },
+  {
+    "revision": "3be1b8d3de338a71cc2076993e85df19",
+    "url": "/img/com.laalaguer.token-transfer.3be1b8d3.png"
+  },
+  {
+    "revision": "017d00adee0ab1aece6e2d32c6e1fb07",
+    "url": "/img/com.non-fungibulls.017d00ad.png"
+  },
+  {
+    "revision": "352a3ed77e9298033df41d7250ca6a08",
+    "url": "/img/com.sharksofanarchy.352a3ed7.png"
+  },
+  {
+    "revision": "9277741fdc3e98c1c9bc1af583c86c2b",
+    "url": "/img/com.smuzzies.3dables.9277741f.png"
+  },
+  {
+    "revision": "3145543551af25e1f9a1179b2e32dd75",
+    "url": "/img/com.theinkaempire.31455435.png"
+  },
+  {
+    "revision": "b37ae9e98b7e3f8dbb62e778971ff4ed",
+    "url": "/img/com.vechain.bmac.b37ae9e9.png"
+  },
+  {
+    "revision": "5b9cf491e79d47c158776e561efe350f",
+    "url": "/img/com.vechain.insight.5b9cf491.png"
+  },
+  {
+    "revision": "c5c7f935831497cb0cc36f3c335b70a2",
+    "url": "/img/com.vechain.inspector.c5c7f935.png"
+  },
+  {
+    "revision": "2b44a9bb435e238743804c85a2b297f4",
+    "url": "/img/com.vechain.tokens.2b44a9bb.png"
+  },
+  {
+    "revision": "179459d5410eed9167e21bbd80675cbf",
+    "url": "/img/com.vechain.vevote.179459d5.png"
+  },
+  {
+    "revision": "1171a6a7122fcce28f52a15ca2097eb9",
+    "url": "/img/com.vechainstats.1171a6a7.png"
+  },
+  {
+    "revision": "9598d159530f69a9ae6ad43061a2c04f",
+    "url": "/img/com.vechainstats.manager.9598d159.png"
+  },
+  {
+    "revision": "718c7f242ea0163d467dd2573d57de40",
+    "url": "/img/com.vefam.718c7f24.png"
+  },
+  {
+    "revision": "9c0564d64ff489114f032b24125861b5",
+    "url": "/img/com.vefam.pixelpuffs.9c0564d6.png"
+  },
+  {
+    "revision": "14e3d7969e72de40bf834b6708231845",
+    "url": "/img/com.vefam.vpunks-weapons.14e3d796.png"
+  },
+  {
+    "revision": "b27721c0f9aef8960a173feb782d464f",
+    "url": "/img/com.venonymous.b27721c0.png"
+  },
+  {
+    "revision": "6da506af46c1b3c23929bd8b8de0614d",
+    "url": "/img/com.verocket.app.6da506af.png"
+  },
+  {
+    "revision": "6c02a3bbe500c3deb0f2ecc58741ff14",
+    "url": "/img/com.vexchange.vexchange.6c02a3bb.png"
+  },
+  {
+    "revision": "6a009ff0b4506ea173f44544a4d73390",
+    "url": "/img/com.vns.vns.6a009ff0.png"
+  },
+  {
+    "revision": "1455e687b55d62b7e8a13dc155794c10",
+    "url": "/img/com.worldofv.expluslottery.1455e687.png"
+  },
+  {
+    "revision": "8de6b28ab2274e30897430c6df96f143",
+    "url": "/img/com.worldofv.marketplace-nft.8de6b28a.png"
+  },
+  {
+    "revision": "3c4e6c6fb960bb7693863daf6005690c",
+    "url": "/img/com.worldofv.staking.3c4e6c6f.png"
+  },
+  {
+    "revision": "493f4651af409580a03da93cc0454e09",
+    "url": "/img/com.worldofv.vehashes.493f4651.png"
+  },
+  {
+    "revision": "bbce63d6dbde04d887e2e04c64d1e982",
+    "url": "/img/domains.vet.bbce63d6.png"
+  },
+  {
+    "revision": "f7e2b4a682a7611255c81b3ab8293e96",
+    "url": "/img/energy.vechain.f7e2b4a6.png"
+  },
+  {
+    "revision": "dc81d5dd6e05f74e1b758822e3afce3b",
+    "url": "/img/energy.vechain.ramp.dc81d5dd.png"
+  },
+  {
+    "revision": "1e78cddb6588452cee49b61f1830b72c",
+    "url": "/img/energy.vechain.swap.1e78cddb.png"
+  },
+  {
+    "revision": "88ea9403f2f58f2776985d4c8a5f70fd",
+    "url": "/img/energy.vechain.tools.wipe.88ea9403.png"
+  },
+  {
+    "revision": "13509a2fa21a4f078872f94654d63429",
+    "url": "/img/exchange.vtho.13509a2f.png"
+  },
+  {
+    "revision": "bd2b75315c542dbf2f323a8a523746aa",
+    "url": "/img/io.adexr.app.bd2b7531.png"
+  },
+  {
+    "revision": "e8e25e34fc1e539f4bd92c821b053b9d",
+    "url": "/img/io.dthor.e8e25e34.png"
+  },
+  {
+    "revision": "5fca8c6ec09dd70287caccad06e53807",
+    "url": "/img/io.exoworlds.5fca8c6e.png"
+  },
+  {
+    "revision": "68aebefccf7dfcdb5057f29cfd8b6135",
+    "url": "/img/io.gangstergorillaz.68aebefc.png"
+  },
+  {
+    "revision": "ecc4e9fb6514416d3be070261297e8ea",
+    "url": "/img/io.mvanft.ecc4e9fb.png"
+  },
+  {
+    "revision": "00e8883574fef8beace3843fe53fed1d",
+    "url": "/img/io.realitems.00e88835.png"
+  },
+  {
+    "revision": "166ac8066a159d5f2e05d8bfea5c4562",
+    "url": "/img/io.safeswap.app.166ac806.png"
+  },
+  {
+    "revision": "a5fce99e604efa65ac28c00459cfa365",
+    "url": "/img/io.thorblock.portal.a5fce99e.png"
+  },
+  {
+    "revision": "b877969d0eaca9ce497faa1c9ddc20ea",
+    "url": "/img/io.vesea.doppelganger.b877969d.png"
+  },
+  {
+    "revision": "ef364cdcca4c8038a39d233df55ea50d",
+    "url": "/img/io.vesea.ef364cdc.png"
+  },
+  {
+    "revision": "c7c9721b26e6b6a4946e393b6408db71",
+    "url": "/img/io.vesea.forestnationkeepers.c7c9721b.png"
+  },
+  {
+    "revision": "6b8d6ccb881af52d2d42fa5e81efb2c5",
+    "url": "/img/io.vesea.funkysalamanders.6b8d6ccb.png"
+  },
+  {
+    "revision": "2144cd9c410bea69f953a95b346fc194",
+    "url": "/img/io.vesea.galaxy.2144cd9c.png"
+  },
+  {
+    "revision": "5cc316a9233bb768f7fc346c806cb1e0",
+    "url": "/img/io.vesea.goatzclub.5cc316a9.png"
+  },
+  {
+    "revision": "d771a429e7443877e2b2f9e80c09478f",
+    "url": "/img/io.vesea.guardians.d771a429.png"
+  },
+  {
+    "revision": "c6722302e5abc5aff85d95650049d8dd",
+    "url": "/img/io.vesea.kickbackkoalas.c6722302.png"
+  },
+  {
+    "revision": "533d16a2972001db6152998da8d53ac3",
+    "url": "/img/io.vesea.kings.533d16a2.png"
+  },
+  {
+    "revision": "5933985c5cd3bdcc9789bc1fb2121f36",
+    "url": "/img/io.vesea.madvapes.5933985c.png"
+  },
+  {
+    "revision": "4c296e927714be39f3b2e77997856eab",
+    "url": "/img/io.vesea.metaversials.4c296e92.png"
+  },
+  {
+    "revision": "6d8c4b0645a56cf7fbbeaca23611bc91",
+    "url": "/img/io.vesea.minomob.6d8c4b06.png"
+  },
+  {
+    "revision": "7a7d9f7b776fb2f43714f269559853bf",
+    "url": "/img/io.vesea.nonfungiblebookclub.7a7d9f7b.png"
+  },
+  {
+    "revision": "40a807dbffa9803f979151df43140b58",
+    "url": "/img/io.vesea.npo.40a807db.png"
+  },
+  {
+    "revision": "b79c6e590facc1d7e625383ec5b2647f",
+    "url": "/img/io.vesea.pixeloutlaws.b79c6e59.png"
+  },
+  {
+    "revision": "3de36d01c058bbb81ef0288f6d3a4ea5",
+    "url": "/img/io.vesea.recycler.3de36d01.png"
+  },
+  {
+    "revision": "32d2f967121367de01a431819830ff48",
+    "url": "/img/io.vesea.smuzzies.32d2f967.png"
+  },
+  {
+    "revision": "ec5d0142fd35099d8496221079817f0f",
+    "url": "/img/io.vesea.stardustspewctres.ec5d0142.png"
+  },
+  {
+    "revision": "bcfd2b979a6736b0541a2c5639c69bd1",
+    "url": "/img/io.vesea.universe.bcfd2b97.png"
+  },
+  {
+    "revision": "5f6858b471eb7f7a3c1e8ffb33f8c89a",
+    "url": "/img/io.vesea.veabstract.5f6858b4.png"
+  },
+  {
+    "revision": "e1a36ba3452da7d4947529acc2587000",
+    "url": "/img/io.vesea.veeparrots.e1a36ba3.png"
+  },
+  {
+    "revision": "9ddf91421c924c573a15b08a41610a03",
+    "url": "/img/io.vesea.veghosts.9ddf9142.png"
+  },
+  {
+    "revision": "6c5a6513c78b76e4c7c33f3a6589ac61",
+    "url": "/img/io.vesea.vegnomes.6c5a6513.png"
+  },
+  {
+    "revision": "95847b499ca2b2b0f7217b76df440bb1",
+    "url": "/img/io.vesea.vekings.95847b49.png"
+  },
+  {
+    "revision": "6e4b60a88a5b17d150b137acef3d0f72",
+    "url": "/img/io.vesea.veloot.6e4b60a8.png"
+  },
+  {
+    "revision": "2ec7f322bdd566214609ca4317a78dbe",
+    "url": "/img/io.vesea.venerds.2ec7f322.png"
+  },
+  {
+    "revision": "2c65b7d251d4b0164110ab444b683c1c",
+    "url": "/img/io.vesea.verocket.2c65b7d2.png"
+  },
+  {
+    "revision": "902243911b700f736b385561024eaa76",
+    "url": "/img/io.vesea.veskullz.90224391.png"
+  },
+  {
+    "revision": "93959008fd74b44506244cf9ad4207fd",
+    "url": "/img/io.vesea.veysarum.93959008.png"
+  },
+  {
+    "revision": "36c817280f79c9f2a8c052e6d81745c4",
+    "url": "/img/io.vesea.vfoxes.36c81728.png"
+  },
+  {
+    "revision": "bc883db566155dfcd9c477bfff862679",
+    "url": "/img/io.vesea.victs.bc883db5.png"
+  },
+  {
+    "revision": "28adab7e51a84ef015c7eb9e22be19ad",
+    "url": "/img/io.vesea.vumanoids.28adab7e.png"
+  },
+  {
+    "revision": "165d558fcb3d34cc51a73af8fe606462",
+    "url": "/img/io.vesea.vvardogs.165d558f.png"
+  },
+  {
+    "revision": "0ec2c1df0ec62cf965401953d39e5c87",
+    "url": "/img/io.vesea.zillyzombies.0ec2c1df.png"
+  },
+  {
+    "revision": "90b2fe9334b0865de779021f505cbc66",
+    "url": "/img/io.wallofvame.90b2fe93.png"
+  },
+  {
+    "revision": "bd8ff73cf60f9860d954f31533ed5269",
+    "url": "/img/network.xp.bridge.bd8ff73c.png"
+  },
+  {
+    "revision": "789eba102f6fbb921efcdd47d66cb943",
+    "url": "/img/org.favo.envelop.789eba10.png"
+  },
+  {
+    "revision": "e680b3b975966dab9d3c0fd491790316",
+    "url": "/img/org.vebetterdao.connect.e680b3b9.png"
+  },
+  {
+    "revision": "e680b3b975966dab9d3c0fd491790316",
+    "url": "/img/org.vebetterdao.e680b3b9.png"
+  },
+  {
+    "revision": "e680b3b975966dab9d3c0fd491790316",
+    "url": "/img/org.vebetterdao.governance.e680b3b9.png"
+  },
+  {
+    "revision": "37e14619a5e0ba023e69353ac4588edb",
+    "url": "/img/org.vechain.rewards.app.37e14619.png"
+  },
+  {
+    "revision": "8717fa3340361ab090c8ed1a2214d06c",
+    "url": "/img/pro.oceanex.diver.8717fa33.png"
+  },
+  {
+    "revision": "5d2ffb6326292621f1dab7a58cf95713",
+    "url": "/img/pro.oceanex.nft.5d2ffb63.png"
+  },
+  {
+    "revision": "fca5c59db824b4c59abc22da99c52029",
+    "url": "/img/pro.oceanex.nftgallery.fca5c59d.png"
+  },
+  {
+    "revision": "32da6ec6b136435a4caa4d900a52f63f",
+    "url": "/img/pro.oceanex.nftgallerypro.32da6ec6.png"
+  },
+  {
+    "revision": "171313ba1af7c988a5fd88d4c544dd16",
+    "url": "/img/tools.wovnft.171313ba.png"
+  },
+  {
+    "revision": "e5b502d17c5d56a5b74a064a6a6648d4",
+    "url": "/img/vechain-app-hub.e5b502d1.png"
+  },
+  {
+    "revision": "2c453094af52e013ac0a25b0d4bbcf86",
+    "url": "/img/vet.cleanify.2c453094.png"
+  },
+  {
+    "revision": "5dc5140ae834c62385d0151db8af80b5",
+    "url": "/img/vet.mugshot.5dc5140a.png"
+  },
+  {
+    "revision": "8858103e0f6f6b8987aa6f325c25360a",
+    "url": "/img/xyz.velottery.8858103e.png"
+  },
+  {
+    "revision": "3d248b63121c3f3b8065651ec29ec529",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b66597b2a54e69cfa1c",
+    "url": "/js/app.5454f46e.js"
+  },
+  {
+    "revision": "ebeca7bfc5b2ee802d43",
+    "url": "/js/chunk-vendors.c922ea9d.js"
+  },
+  {
+    "revision": "049d2f5048faba6366600bd3c467b498",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
